671e7246f8350048e2233d6b0275d44d642a4955
Mashnar <szymon.fr@gmail.com>
2018-10-10 09:24:44 +0200

    Autoryzacja Google

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+    @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+             
+
+                     
+
+                       
+                                                              <a href="{{ url('/redirect') }}" class="btn btn-primary">Login With Google</a>
+                          
+                               
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
